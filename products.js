29ae39f9934fd96593f7d13e8801b6b18a881662
ffdhas <alyawidury2009@gmail.com>
2026-05-27 17:01:46 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,8 @@
+{
+  id       : 1,
+  name     : "Nama Produk",
+  image    : "https://link-gambar.jpg",
+  price    : 45000,
+  status   : "ready",   // atau "sold"
+  category : "Daging"   // pilih salah satu
+}
